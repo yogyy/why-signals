a53cdff0999e191a28aea8d79ea85888ae52f0cd
yogyy <yogimr261@gmail.com>
2024-06-01 18:24:59 +0700

    feat: add signals

---
+++ signals.js
@@ -0,0 +1,33 @@
+let subscriber = null;
+
+export function signal(value) {
+  const subscriptions = new Set();
+
+  return {
+    get value() {
+      if (subscriber) {
+        subscriptions.add(subscriber);
+      }
+      return value;
+    },
+    set value(updated) {
+      value = updated;
+      subscriptions.forEach((fn) => fn());
+    },
+  };
+}
+
+export function effect(fn) {
+  subscriber = fn;
+  fn();
+  subscriber = null;
+}
+
+export function derive(fn) {
+  const derived = signal();
+  effect(() => {
+    derived.value = fn();
+  });
+
+  return derived;
+}
